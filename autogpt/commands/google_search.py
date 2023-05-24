39b5b54811d342201b5a0555646bc63ce9e25305
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2023-05-25 00:58:43 +0200

    update ddg

--- autogpt/commands/google_search.py
+++ autogpt/commands/google_search.py
@@ -1,9 +1,10 @@
 """Google search command for Autogpt."""
 from __future__ import annotations
+from itertools import islice
 
 import json
 
-from duckduckgo_search import ddg
+from duckduckgo_search import DDGS
 
 from autogpt.commands.command import command
 from autogpt.config import Config
@@ -13,45 +14,9 @@ from googleapiclient.errors import HttpError
 
 global_config = Config()
 
-
-# @command("google", "Google Search", '"query": "<query>"', not bool(global_config.google_api_key))
-# def google_search(query: str, num_results: int = 8, **kwargs) -> str:
-#     """Return the results of a Google search
-
-#     Args:
-#         query (str): The search query.
-#         num_results (int): The number of results to return.
-
-#     Returns:
-#         str: The results of the search.
-#     """
-#     search_results = []
-#     if not query:
-#         return json.dumps(search_results)
-
-#     results = ddg(query, max_results=num_results)
-#     if not results:
-#         return json.dumps(search_results)
-
-#     for j in results:
-#         search_results.append(j)
-
-#     results = json.dumps(search_results, ensure_ascii=False, indent=4)
-#     return safe_google_results(results)
-
-
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-@command(
-    "google",
-    "Google Search",
-    '"query": "<query>"',
-    bool(global_config.google_api_key) and bool(global_config.custom_search_engine_id),
-    "Configure google_api_key and custom_search_engine_id.",
-)
-def google_official_search(query: str, num_results: int = 8, **kwargs) -> str | list[str]:
-    """Return the results of a Google search using the official Google API
+@command("google", "Google Search", '"query": "<query>"')
+def google_search(query: str, num_results: int = 8) -> str:
+    """Return the results of a Google search
 
     Args:
         query (str): The search query.
@@ -60,45 +25,80 @@ def google_official_search(query: str, num_results: int = 8, **kwargs) -> str | 
     Returns:
         str: The results of the search.
     """
+    search_results = []
+    if not query:
+        return json.dumps(search_results)
 
-    try:
-        # Get the Google API key and Custom Search Engine ID from the config file
-        api_key = global_config.google_api_key
-        custom_search_engine_id = global_config.custom_search_engine_id
+    results = DDGS().text(query)
+    if not results:
+        return json.dumps(search_results)
 
-        # Initialize the Custom Search API service
-        service = build("customsearch", "v1", developerKey=api_key)
+    for item in islice(results, num_results):
+        search_results.append(item)
 
-        # Send the search query and retrieve the results
-        result = (
-            service.cse()
-            .list(q=query, cx=custom_search_engine_id, num=num_results)
-            .execute()
-        )
+    results = json.dumps(search_results, ensure_ascii=False, indent=4)
+    return safe_google_results(results)
 
-        # Extract the search result items from the response
-        search_results = result.get("items", [])
 
-        # Create a list of only the URLs from the search results
-        search_results_links = [f"{item['link']} {item['title']}: {item['snippet']}" for item in search_results]
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
 
-    except HttpError as e:
-        # Handle errors in the API call
-        error_details = json.loads(e.content.decode())
+# @command(
+#     "google",
+#     "Google Search",
+#     '"query": "<query>"',
+#     bool(global_config.google_api_key) and bool(global_config.custom_search_engine_id),
+#     "Configure google_api_key and custom_search_engine_id.",
+# )
+# def google_official_search(query: str, num_results: int = 8, **kwargs) -> str | list[str]:
+#     """Return the results of a Google search using the official Google API
 
-        # Check if the error is related to an invalid or missing API key
-        if error_details.get("error", {}).get(
-            "code"
-        ) == 403 and "invalid API key" in error_details.get("error", {}).get(
-            "message", ""
-        ):
-            return "Error: The provided Google API key is invalid or missing."
-        else:
-            return f"Error: {e}"
-    # google_result can be a list or a string depending on the search results
+#     Args:
+#         query (str): The search query.
+#         num_results (int): The number of results to return.
 
-    # Return the list of search result URLs
-    return safe_google_results("\n".join(search_results_links))
+#     Returns:
+#         str: The results of the search.
+#     """
+
+#     try:
+#         # Get the Google API key and Custom Search Engine ID from the config file
+#         api_key = global_config.google_api_key
+#         custom_search_engine_id = global_config.custom_search_engine_id
+
+#         # Initialize the Custom Search API service
+#         service = build("customsearch", "v1", developerKey=api_key)
+
+#         # Send the search query and retrieve the results
+#         result = (
+#             service.cse()
+#             .list(q=query, cx=custom_search_engine_id, num=num_results)
+#             .execute()
+#         )
+
+#         # Extract the search result items from the response
+#         search_results = result.get("items", [])
+
+#         # Create a list of only the URLs from the search results
+#         search_results_links = [f"{item['link']} {item['title']}: {item['snippet']}" for item in search_results]
+
+#     except HttpError as e:
+#         # Handle errors in the API call
+#         error_details = json.loads(e.content.decode())
+
+#         # Check if the error is related to an invalid or missing API key
+#         if error_details.get("error", {}).get(
+#             "code"
+#         ) == 403 and "invalid API key" in error_details.get("error", {}).get(
+#             "message", ""
+#         ):
+#             return "Error: The provided Google API key is invalid or missing."
+#         else:
+#             return f"Error: {e}"
+#     # google_result can be a list or a string depending on the search results
+
+#     # Return the list of search result URLs
+#     return safe_google_results("\n".join(search_results_links))
 
 
 def safe_google_results(results: str | list) -> str:
